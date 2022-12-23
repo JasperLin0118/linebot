45206355ce1b1a1d98a171b8f73d06321a832ccf
Jasper Lin <73963814+JasperLin0118@users.noreply.github.com>
2022-12-24 00:17:30 +0800

    delete some redundant states

--- fsm.py
+++ fsm.py
@@ -45,31 +45,48 @@ class TocMachine(GraphMachine):
         self.advance(event)
 
     def is_going_to_main_menu(self, event):
+        text = event.message.text
+        reply_token = event.reply_token
+        if(text == "address"):
+            show_address(reply_token)
+        elif(text == "contact number"):
+            show_contact_number(reply_token)
+        elif(text == "show FSM"):
+            show_FSM(reply_token)
+            if(self.state != "main_menu"):
+                return True
+            return False
+        elif(text == "maintenance method"):
+            show_maintenance_method(reply_token)
+            if(self.state != "main_menu"):
+                return True
+            return False
+        elif(text == "office tables"):
+            show_office_tables(reply_token)
+            return False
+        elif(text == "office chairs and sofas"):
+            show_office_chairs(reply_token)
+            return False
+        elif(text == "contents"):
+            show_contents(reply_token)
+            return False
+        elif(text == "search style or category"):
+            show_search_style_or_category(reply_token)
+        elif(text == "contents and images"):
+            show_search_contents_and_images(reply_token)
+        elif(text == "contact us"):
+            show_contact_us(reply_token)
+        elif(text == "main menu"):
+            show_enter_menu(reply_token)
         return True
     
     def on_enter_main_menu(self, event):
         print("I'm entering menu")
 
-    def is_staying_at_main_menu(self, event):
-        if(event.message.text == "show FSM"):
-            show_FSM(event.reply_token)
-        elif(event.message.text == "maintenance method"):
-            show_maintenance_method(event.reply_token)
-        return True
-
     def is_going_to_contact_us(self, event):
         text = event.message.text
         return text.lower() == "contact us"
     
-    def is_staying_at_contact_us(self, event):
-        reply_token = event.reply_token
-        text = event.message.text
-        if(text == "address"):
-            show_address(reply_token)
-        elif(text == "contact number"):
-            show_contact_number(reply_token)
-        return True
-
     def on_enter_contact_us(self, event):
         print("I'm entering contact_us")
         reply_token = event.reply_token
@@ -83,40 +100,6 @@ class TocMachine(GraphMachine):
         print("I'm entering contents_and_images")
         reply_token = event.reply_token
         show_search_contents_and_images(reply_token)
-    
-    def is_going_to_idle(self, event):
-        text = event.message.text
-        return text.lower() == "leave main menu"
-    
-    def is_going_to_contents(self, event):
-        text = event.message.text
-        return text.lower() == "contents"
-    
-    def on_enter_contents(self, event):
-        print("I'm entering contents")
-        reply_token = event.reply_token
-        show_contents(reply_token)
-        self.advance(event)
-    
-    def is_going_to_office_chairs_and_sofas(self, event):
-        text = event.message.text
-        return text.lower() == "office chairs and sofas"
-    
-    def on_enter_office_chairs_and_sofas(self, event):
-        print("I'm entering office_chairs")
-        reply_token = event.reply_token
-        show_office_chairs(reply_token)
-        self.advance(event)
-        
-    def is_going_to_office_tables(self, event):
-        text = event.message.text
-        return text.lower() == "office tables"
-    
-    def on_enter_office_tables(self, event):
-        print("I'm entering office_tables")
-        reply_token = event.reply_token
-        show_office_tables(reply_token)
-        self.advance(event)
 
     def on_exit_menu(self):
         print("Leaving menu")
